c9e1d03d97a0881a4547a4e73d924c3bbc3fd2d2
wangchaoduo <573746388@qq.com>
2018-05-07 10:55:27 +0800

    0.0.2

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,18 @@
-declare namespace goobyehx {
-
-    class Component {
-        constructor(id: string, fields: string[]);
-    }
+export default goodbyehx;
+export as namespace goodbyehx;
+declare namespace goodbyehx {
+  class Component {
+    constructor(id: string, fields: string[]);
+  }
+  let el: (elementName: string, ...any) => HTMLElement;
+  interface store {
+    state: any;
+    listener: any;
+    getState: () => any;
+    init: (Object) => void;
+    listen: (id: string, fields: string[], callback: Function) => void;
+    dispatch: (newState: any) => void;
+  }
+  let getState: () => any;
+  let createApp: (id: string, App: any) => void;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,79 @@
-import Component from './Component'
+import _Component from './Component'
 import _store, { getState as _getState } from './store'
-export { createApp } from './utils'
+import { createApp } from './utils'
 
 const attributeExceptions = [
-    `role`,
+  `role`,
 ];
 
 function appendText(el, text) {
-    const textNode = document.createTextNode(text);
-    el.appendChild(textNode);
+  const textNode = document.createTextNode(text);
+  el.appendChild(textNode);
 }
 
 function appendArray(el, children) {
-    children.forEach((child) => {
-        if (Array.isArray(child)) {
-            appendArray(el, child);
-        } else if (child instanceof window.Element) {
-            el.appendChild(child);
-        } else if (typeof child === `string`) {
-            appendText(el, child);
-        }
-    });
+  children.forEach((child) => {
+    if (Array.isArray(child)) {
+      appendArray(el, child);
+    } else if (child instanceof Element) {
+      el.appendChild(child);
+    } else if (typeof child === `string`) {
+      appendText(el, child);
+    }
+  });
 }
 
 function setStyles(el, styles) {
-    if (!styles) {
-        el.removeAttribute(`styles`);
-        return;
-    }
+  if (!styles) {
+    el.removeAttribute(`styles`);
+    return;
+  }
 
-    Object.keys(styles).forEach((styleName) => {
-        if (styleName in el.style) {
-            el.style[styleName] = styles[styleName]; // eslint-disable-line no-param-reassign
-        } else {
-            console.warn(`${styleName} is not a valid style for a <${el.tagName.toLowerCase()}>`);
-        }
-    });
+  Object.keys(styles).forEach((styleName) => {
+    if (styleName in el.style) {
+      el.style[styleName] =
+        styles[styleName];  // eslint-disable-line no-param-reassign
+    } else {
+      console.warn(`${styleName} is not a valid style for a <${
+        el.tagName.toLowerCase()}>`);
+    }
+  });
 }
 
 function makeElement(type, textOrPropsOrChild?, ...otherChildren) {
-    const el = document.createElement(type);
-    if (Array.isArray(textOrPropsOrChild)) {
-        appendArray(el, textOrPropsOrChild);
-    } else if (textOrPropsOrChild instanceof window.Element) {
-        el.appendChild(textOrPropsOrChild);
-    } else if (typeof textOrPropsOrChild === `string`) {
-        appendText(el, textOrPropsOrChild);
-    } else if (typeof textOrPropsOrChild === `object`) {
-        Object.keys(textOrPropsOrChild).forEach((propName) => {
-            if (propName in el || attributeExceptions.indexOf(propName) !== -1) {
-                const value = textOrPropsOrChild[propName];
+  const el = document.createElement(type);
+  if (Array.isArray(textOrPropsOrChild)) {
+    appendArray(el, textOrPropsOrChild);
+  } else if (textOrPropsOrChild instanceof Element) {
+    el.appendChild(textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `string`) {
+    appendText(el, textOrPropsOrChild);
+  } else if (typeof textOrPropsOrChild === `object`) {
+    Object.keys(textOrPropsOrChild).forEach((propName) => {
+      if (propName in el || attributeExceptions.indexOf(propName) !== -1) {
+        const value = textOrPropsOrChild[propName];
 
-                if (propName === `style`) {
-                    setStyles(el, value);
-                } else if (value) {
-                    el[propName] = value;
-                }
-            } else {
-                console.warn(`${propName} is not a valid property of a <${type}>`);
-            }
-        });
-    }
+        if (propName === `style`) {
+          setStyles(el, value);
+        } else if (value) {
+          el[propName] = value;
+        }
+      } else {
+        console.warn(`${propName} is not a valid property of a <${type}>`);
+      }
+    });
+  }
 
-    if (otherChildren) appendArray(el, otherChildren);
-    return el;
+  if (otherChildren) appendArray(el, otherChildren);
+  return el;
 }
 
-export const el = (elementName, ...args) => makeElement(elementName, ...args)
-export default Component
-export const store = _store
-export const getState = _getState
+const app = {
+  el: (elementName, ...args) => makeElement(elementName, ...args),
+  Component: _Component,
+  store: _store,
+  getState: _getState,
+  createApp: createApp
+}
+
+export default app;
